364773356c22b80a27d2135262e04fe2808015da
sunda12345667 <sundayadenuga320@gmail.com>
2023-12-04 06:30:02 -0800

    python project

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #ifndef LISTS_H
 #define LISTS_H
 
@@ -7,7 +8,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *
+ * for project
  */
 typedef struct listint_s
 {
@@ -19,6 +20,6 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
-listint_t *insert_node(listint_t **head, int number);
+int is_palindrome(listint_t **head);
 
 #endif /* LISTS_H */
