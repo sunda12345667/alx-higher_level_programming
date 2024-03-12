ffa1384726e7f98d22cafad5319cd8532bfc0e31
sunda12345667 <sundayadenuga320@gmail.com>
2024-03-12 04:16:57 -0700

    Jacecript objects scropes and closures

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,11 @@
+5-square.js
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+
+module.exports = Square;
